73fe3de1bf36eee971a02da02c409c0f9154dc61
Malong-max <mls_0220@qq.com>
2026-05-14 18:42:08 +0800

    add value a = int

--- h.py
+++ h.py
@@ -5,4 +5,5 @@ print("this is the third line and there is a demo")
 print("there are some error in the code")
 print("分支可以有中文还可以有/但是不能有空格")
 print("使用demo进行git提交")
-print("使用beta分支进行git提交")
+print("使用beta分支进行git提交") 
+a = 1213
